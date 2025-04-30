06968160f492fcb6b859e7e5cff2820fd348aca0
adithya-sunoj <sunojadithya@gmail.com>
2025-04-30 09:28:22 +0400

    updates

--- src/main.py
+++ src/main.py
@@ -2,5 +2,9 @@ import streamlit as st
 from streamlit_cropper import st_cropper
 import pandas as pd
 from PIL import Image, ImageEnhance, ImageOps
-from model.model import SimbleModel
+from tensorflow.keras.models import load_model
+import tensorflow as tf
 import numpy as np
+
+MODEL_PATH = "..models/asl_custom_cnn.h5"
+ALPNAMES_PATH = "model/alphabetnames.txt"
