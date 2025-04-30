423d4abf04c115f90c55744eab1d50c291b8477c
adithya-sunoj <sunojadithya@gmail.com>
2025-04-30 07:54:18 +0400

    updates

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
+from streamlit_cropper import st_cropper
 import pandas as pd
+from PIL import Image, ImageEnhance, ImageOps
+from model.model import SimbleModel
 import numpy as np
-import matplotlib.pyplot as plt
-print("testing")   
